2b8bd3ffd3d6193e1d54f1d8c0ab5d22b213062a
bquinn83 <45705195+bquinn83@users.noreply.github.com>
2020-05-18 19:40:45 -0600

    Create seperate project Views
    Remove Index
    Create MyProjects
    Create AllProjects
    Modify ProjectViewModels

--- ViewModels/ProjectsViewModels.cs
+++ ViewModels/ProjectsViewModels.cs
@@ -8,7 +8,9 @@ namespace KillBug.ViewModels
 {
     public class DeveloperAssignmentsViewModel
     {
+        public string MenuDrop { get; set; }
         public string Name { get; set; }
+        public string AvatarPath { get; set; }
         public string Email { get; set; }
         public List<string> Projects { get; set; }
     }
