8211e670e2b41689d709bca4ff8a12dd92f2e50e
bquinn83 <45705195+bquinn83@users.noreply.github.com>
2020-05-28 00:52:55 -0600

    ROUTING: Update default route

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace KillBug
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Dashboard", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
