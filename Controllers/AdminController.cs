2c6a0c3baaf17dbbf64b6dd57ce5d65c26b62c81
bquinn83 <45705195+bquinn83@users.noreply.github.com>
2020-06-04 22:39:54 -0600

    UPDATE: Admin
    Add JSON action UserRoleUpdate
    Add AJAX call to Manage Roles
    Update UserRolesVM
    Update 'Multi Edit'
    Add toastr Alerts for AJAX actions

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using KillBug.Classes;
 using KillBug.Models;
 using KillBug.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.EnterpriseServices;
@@ -9,14 +11,18 @@ using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI.WebControls;
 
 namespace KillBug.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
-        private readonly UserRolesHelper RolesHelper = new UserRolesHelper();
         private ApplicationDbContext db = new ApplicationDbContext();
+        private UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+        private readonly UserRolesHelper RolesHelper = new UserRolesHelper();
         private NotificationHelper notifications = new NotificationHelper();
 
         //GET: TicketSettings
@@ -32,26 +38,33 @@ namespace KillBug.Controllers
             return View(model);
         }
         //GET: ManageRoles
+        [Authorize(Roles="Admin")]
         public ActionResult ManageRoles()
         {
+
             var users = db.Users.ToList();
             var viewData = new List<UserRoleVM>();
 
             foreach (var user in users)
             {
-                viewData.Add(new UserRoleVM
+                if (user.Id != User.Identity.GetUserId())
                 {
-                    FirstName = user.FirstName,
-                    LastName = user.LastName,
-                    Email = user.Email,
-                    Role = RolesHelper.ListUserRoles(user.Id).FirstOrDefault() ?? "None"
-                });
+                    viewData.Add(new UserRoleVM
+                    {
+                        FirstName = user.FirstName,
+                        LastName = user.LastName,
+                        Email = user.Email,
+                        Roles = new SelectList(db.Roles, "Name", "Name", user.UserRole())
+                    });
+                }
             }
-            ViewBag.UserIds = new MultiSelectList(db.Users, "Id", "Email");
+
+            ViewBag.UserIds = new MultiSelectList(db.Users.OrderBy(u => u.LastName), "Id", "FullNamePosition");
             ViewBag.RoleName = new SelectList(db.Roles, "Name", "Name");
 
             return View(viewData);
         }
+
         //POST: ManageRoles
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -73,6 +86,29 @@ namespace KillBug.Controllers
                 }
             }
             return RedirectToAction("ManageRoles");
+        }
+
+        // AJAX POST: UpdateUserRole
+        [HttpPost]
+        public JsonResult UpdateUserRole(string email, string roleName)
+        {
+            try
+            {
+                var user = db.Users.Where(u => u.Email == email).FirstOrDefault();
+                userManager.RemoveFromRole(user.Id, user.UserRole());
+                userManager.AddToRole(user.Id, roleName);
+
+                // Before returning, we must do some housekeeping:
+                // If your downgrading a Project Manager, they should be removed as Project Manager for all Projects
+                // If your upgrading a developer, should their tickets be re-assigned? Thats up to you!
+
+                return Json(true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return Json(false);
+            }
         }
         // GET: Admin
         public ActionResult AskAboutRoles()
--- ViewModels/AdminViewModels.cs
+++ ViewModels/AdminViewModels.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using KillBug.Models;
 
 namespace KillBug.ViewModels
@@ -11,7 +12,7 @@ namespace KillBug.ViewModels
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string Role { get; set; }
+        public IEnumerable<SelectListItem> Roles { get; set; }
     }
     public class TicketSettingsVM
     {
